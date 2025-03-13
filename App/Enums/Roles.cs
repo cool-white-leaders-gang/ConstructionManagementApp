48df6a9b2c4c2bf374365786edcfde8f2bfeb022
Filip Lubka <filip.lubka2007@gmail.com>
2025-03-13 14:42:05 +0100

    Created roles

---
+++ App/Enums/Roles.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionManagementApp.App.Enums
+{
+    //Role użytkowników
+    enum Role   
+    {
+        Admin = 0,
+        Manager = 1,
+        Worker = 2,
+
+    }
+}
