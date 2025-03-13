a722c54add6657c1a4888813b9a7a8d837cd296a
Filip Lubka <filip.lubka2007@gmail.com>
2025-03-13 14:41:35 +0100

    added class user

---
+++ App/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConstructionManagementApp.App.Enums;
+
+namespace ConstructionManagementApp.App.Models
+{
+    //klasa user
+    internal class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+
+        //konstruktor
+        public User(string username, string password, string email, Role role)
+        {
+            Username = username;
+            Email = email;
+            Password = password;
+            Role = role;
+        }
+
+        public string GetAllData()
+        {
+            return $"Username: {Username}, Email: {Email}, Role: {Role}";
+        }
+    }
+}
