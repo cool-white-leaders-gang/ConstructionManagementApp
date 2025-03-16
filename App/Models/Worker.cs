7e515180a694fc0e5c5a1ed2e92c8290e4c876e6
Patryk Stafecki <stafecki303@gmail.com>
2025-03-16 20:55:12 +0100

    Create Worker.cs

---
+++ App/Models/Worker.cs
@@ -0,0 +1,16 @@
+﻿namespace ConstructionManagementApp.App.Models
+{
+    internal class Worker : User
+    {
+        public string Position { get; set; }
+        public Worker(string username, string passwordHash, string email, string position)
+            : base(username, passwordHash, email, Enums.Role.Admin)
+        {
+            Position = position;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Stanowisko: {Position}";
+        }
+    }
+}
