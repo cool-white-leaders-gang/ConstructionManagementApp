67eb440152a34e678337019c60c9d6048bc2db4e
Patryk Stafecki <stafecki303@gmail.com>
2025-03-16 20:53:26 +0100

    Create Manager.cs and update Admin.cs

---
+++ App/Models/Manager.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ConstructionManagementApp.App.Models
+{
+    internal class Manager : User
+    {
+        public string Department {  get; set; }
+        public Manager(string username, string passwordHash, string email, string department)
+            : base(username, passwordHash, email, Enums.Role.Manager)
+        {
+            Department = department;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Dział: {Department}";
+        }
+    }
+}
