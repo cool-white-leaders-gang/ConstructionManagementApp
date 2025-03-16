5e481036effa30315069ac6ac2a4c00c2b9db683
Patryk Stafecki <stafecki303@gmail.com>
2025-03-16 20:49:52 +0100

    Create Admin.cs

---
+++ App/Models/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConstructionManagementApp.App.Models;
+
+namespace ConstructionManagementApp.App.Models
+{
+    internal class Admin : User
+    {
+        public string AdminKey { get; set; }
+        public Admin(string username, string passwordHash, string email, string adminKey)
+            : base(username, passwordHash, email, Enums.Role.Admin)
+        {
+            AdminKey = adminKey;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $", Klucz administracyjny: {AdminKey}";
+        }
+    }
+}
