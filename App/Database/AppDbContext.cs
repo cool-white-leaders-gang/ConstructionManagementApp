0e15600a2c7922cb3118a9d53b9045bc2a7f479b
Filip Lubka <filip.lubka2007@gmail.com>
2025-03-24 11:54:19 +0100

    Added OnModelCreating configuration for Task and TaskAssignment models

--- App/Database/AppDbContext.cs
+++ App/Database/AppDbContext.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using ConstructionManagementApp.App.Models;
 using ConstructionManagementApp.App.Enums;
 using Task = ConstructionManagementApp.App.Models.Task;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ConstructionManagementApp.App.Database
 {
@@ -14,6 +15,7 @@ namespace ConstructionManagementApp.App.Database
     {
         public DbSet<User> Users { get; set; }      //odwołanie do tabeli Users w bazie danych
         public DbSet<Task> Tasks {get;set;}     //odwołanie się do tabeli Tasks w bazie
+        public DbSet<TaskAssignment> TaskAssignments { get; set; }  //odwołanie się do tabeli TaskAssignments w bazie
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)   //elegnackie połączenie z baza danych
         {
             string server = "localhost";
@@ -23,7 +25,7 @@ namespace ConstructionManagementApp.App.Database
             optionsBuilder.UseMySql($"server={server};database={database};user={user};password={password};", new MySqlServerVersion(new Version(10, 5, 9)));
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)  //konfiguracja modelu user, żeby był poprawnie wpisywany
+        protected override void OnModelCreating(ModelBuilder modelBuilder)  //konfiguracja modeli, żeby były poprawnie wpisywane
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<User>(entity =>
@@ -40,6 +42,41 @@ namespace ConstructionManagementApp.App.Database
                       )
                       .IsRequired();
             });
+            
+            
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).IsRequired();
+                entity.Property(e => e.Description).IsRequired();
+                entity.Property(e => e.Priority)
+                      .HasConversion
+                      (
+                          v => v.ToString(),
+                          v => (TaskPriority)Enum.Parse(typeof(TaskPriority), v)
+                      )
+                      .IsRequired();
+                entity.Property(e => e.Progress)
+                      .HasConversion
+                      (
+                          v => v.ToString(),
+                          v => (TaskProgress)Enum.Parse(typeof(TaskProgress), v)
+                      )
+                      .IsRequired();
+            });
+
+            modelBuilder.Entity<TaskAssignment>(entity =>
+            {
+                entity.HasKey(e => new { e.UserId, e.TaskId });
+                entity.HasOne(e => e.user)
+                      .WithMany(e => e.TaskAssignments)
+                      .HasForeignKey(e => e.UserId);
+                entity.HasOne(e => e.task)
+                        .WithMany(e => e.TaskAssignments)
+                        .HasForeignKey(e => e.TaskId);
+            }
+            );
+
         }
     }
 }
