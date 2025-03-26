8338244d5e31bf6e3aff7d516aa35ceace16bcd0
Filip Lubka <filip.lubka2007@gmail.com>
2025-03-26 17:03:25 +0100

    added Enum instead of string to Equipment class

--- App/Models/Equipment.cs
+++ App/Models/Equipment.cs
@@ -1,17 +1,19 @@
 using System;
-
+using ConstructionManagementApp.App.Enums;
 namespace ConstructionManagementApp.App.Models
 {
     internal class Equipment
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Status { get; set; }
+        public EquipmentStatus Status { get; set; }
+        public int ProjectId { get; set; }
 
-        public Equipment(string name, string status)
+        public Equipment(string name, EquipmentStatus status, int projectId)
         {
             Name = name;
             Status = status;
+            ProjectId = projectId;
         }
 
         public override string ToString()
