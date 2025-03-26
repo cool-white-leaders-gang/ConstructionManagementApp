472fbf97aa1b9059b6990b0cc91db6a53d235019
Filip Lubka <filip.lubka2007@gmail.com>
2025-03-26 17:04:29 +0100

    created new join table between teams and users tables

---
+++ App/Models/TeamMembers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructionManagementApp.App.Models
+{
+    internal class TeamMembers
+    {
+        
+        public int TeamId { get; set; }
+        public Team team { get; set; }
+        public int UserId { get; set; }
+        public User user { get; set; }
+    }
+}
