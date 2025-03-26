8cc635f1a867f7393f24246839f4a45ebb5070e9
Patryk Stafecki <stafecki303@gmail.com>
2025-03-26 21:11:56 +0100

    Add constructor to TeamMembers

--- App/Models/TeamMembers.cs
+++ App/Models/TeamMembers.cs
@@ -13,5 +13,13 @@ namespace ConstructionManagementApp.App.Models
         public Team team { get; set; }
         public int UserId { get; set; }
         public User user { get; set; }
+
+        public TeamMembers(int teamId, int userId)
+        {
+            TeamId = teamId;
+            UserId = userId;
+        }
+
+        public TeamMembers() { }
     }
 }
