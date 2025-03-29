f545ddca32f6e5d467a90598a5da3c46826407fc
stafecki <stafecki303@gmail.com>
2025-03-29 20:48:35 +0100

    Fix database

--- import_and_structure/construction_management.sql
+++ import_and_structure/construction_management.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2025 at 05:18 PM
+-- Generation Time: Mar 29, 2025 at 08:45 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `budgets` (
-  `id` int(11) NOT NULL,
-  `totalAmount` decimal(10,2) NOT NULL,
-  `spentAmount` decimal(10,2) NOT NULL
+  `Id` int(11) NOT NULL,
+  `TotalAmount` decimal(10,2) NOT NULL,
+  `SpentAmount` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -40,10 +40,10 @@ CREATE TABLE `budgets` (
 --
 
 CREATE TABLE `equipment` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `projectId` int(11) DEFAULT NULL
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Status` enum('Available','InUse','UnderMaintenance','Broken') NOT NULL,
+  `ProjectId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -53,29 +53,15 @@ CREATE TABLE `equipment` (
 --
 
 CREATE TABLE `issues` (
-  `Id` INT AUTO_INCREMENT PRIMARY KEY,
-    `Title` VARCHAR(255) NOT NULL,
-    `Content` TEXT,
-    `CreatedAt` DATETIME NOT NULL,
-    `CreatedById` INT NOT NULL,
-    `ProjectId` INT NOT NULL,
-    `Priority` ENUM('Low', 'Medium', 'High') NOT NULL,
-    `Status` ENUM('Open', 'In Progress', 'Resolved') NOT NULL DEFAULT 'Open',
-    `ResolvedAt` DATETIME NULL,
-    FOREIGN KEY (`CreatedById`) REFERENCES `Users`(`Id`) ON DELETE CASCADE,
-    FOREIGN KEY (`ProjectId`) REFERENCES `Projects`(`Id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `logs`
---
-
-CREATE TABLE `logs` (
-  `id` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `timestamp` datetime NOT NULL
+  `Id` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Content` text NOT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `CreatedByUserId` int(11) NOT NULL,
+  `ProjectId` int(11) NOT NULL,
+  `Priority` enum('Low','Medium','High') NOT NULL,
+  `Status` enum('Open','InProgress','Resolved') NOT NULL,
+  `ResolvedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -85,24 +71,26 @@ CREATE TABLE `logs` (
 --
 
 CREATE TABLE `materials` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `unit` varchar(50) NOT NULL,
-  `projectId` int(11) DEFAULT NULL
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Quantity` int(11) NOT NULL,
+  `Unit` varchar(50) NOT NULL,
+  `ProjectId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
 --
--- Struktura tabeli dla tabeli `notifications`
+-- Struktura tabeli dla tabeli `progressreports`
 --
 
-CREATE TABLE `notifications` (
-  `id` int(11) NOT NULL,
-  `message` text NOT NULL,
-  `sentAt` datetime NOT NULL,
-  `userId` int(11) DEFAULT NULL
+CREATE TABLE `progressreports` (
+  `Id` int(11) NOT NULL,
+  `Title` varchar(255) NOT NULL,
+  `Content` text NOT NULL,
+  `CreatedAt` datetime NOT NULL,
+  `CreatedByUserId` int(11) NOT NULL,
+  `ProjectId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -112,30 +100,12 @@ CREATE TABLE `notifications` (
 --
 
 CREATE TABLE `projects` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `description` text DEFAULT NULL,
-  `teamId` int(11) DEFAULT NULL,
-  `budgetId` int(11) DEFAULT NULL,
-  `clientId` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Struktura tabeli dla tabeli `reports`
---
-
-CREATE TABLE `reports` (
-  `Id` INT AUTO_INCREMENT PRIMARY KEY,
-    `Title` VARCHAR(255) NOT NULL,
-    `Content` TEXT,
-    `CreatedAt` DATETIME NOT NULL,
-    `CreatedById` INT NOT NULL,
-    `ProjectId` INT NOT NULL,
-    `CompletionPercentage` INT NOT NULL,
-    FOREIGN KEY (`CreatedById`) REFERENCES `Users`(`Id`) ON DELETE CASCADE,
-    FOREIGN KEY (`ProjectId`) REFERENCES `Projects`(`Id`) ON DELETE CASCADE
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) NOT NULL,
+  `Description` text NOT NULL,
+  `TeamId` int(11) DEFAULT NULL,
+  `BudgetId` int(11) DEFAULT NULL,
+  `ClientId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -158,7 +128,7 @@ CREATE TABLE `taskassignments` (
 CREATE TABLE `tasks` (
   `Id` int(11) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `Description` text DEFAULT NULL,
+  `Description` text NOT NULL,
   `Priority` enum('Low','Medium','High') NOT NULL,
   `Progress` enum('New','InProgress','Completed') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -193,12 +163,12 @@ CREATE TABLE `teams` (
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `email` varchar(80) NOT NULL,
-  `passwordHash` char(64) NOT NULL,
-  `role` enum('Admin','Manager','Worker','') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `Id` int(11) NOT NULL,
+  `Username` varchar(30) NOT NULL,
+  `Email` varchar(80) NOT NULL,
+  `PasswordHash` char(64) NOT NULL,
+  `Role` enum('Admin','Manager','Worker','Client') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Indeksy dla zrzutów tabel
@@ -208,59 +178,46 @@ CREATE TABLE `users` (
 -- Indeksy dla tabeli `budgets`
 --
 ALTER TABLE `budgets`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- Indeksy dla tabeli `equipment`
 --
 ALTER TABLE `equipment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `projectId` (`projectId`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `ProjectId` (`ProjectId`);
 
 --
 -- Indeksy dla tabeli `issues`
 --
 ALTER TABLE `issues`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `reportedByUserId` (`reportedByUserId`),
-  ADD KEY `projectId` (`projectId`);
-
---
--- Indeksy dla tabeli `logs`
---
-ALTER TABLE `logs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `CreatedByUserId` (`CreatedByUserId`),
+  ADD KEY `ProjectId` (`ProjectId`);
 
 --
 -- Indeksy dla tabeli `materials`
 --
 ALTER TABLE `materials`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `projectId` (`projectId`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `ProjectId` (`ProjectId`);
 
 --
--- Indeksy dla tabeli `notifications`
+-- Indeksy dla tabeli `progressreports`
 --
-ALTER TABLE `notifications`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `userId` (`userId`);
+ALTER TABLE `progressreports`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `CreatedByUserId` (`CreatedByUserId`),
+  ADD KEY `ProjectId` (`ProjectId`);
 
 --
 -- Indeksy dla tabeli `projects`
 --
 ALTER TABLE `projects`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `teamId` (`teamId`),
-  ADD KEY `budgetId` (`budgetId`),
-  ADD KEY `clientId` (`clientId`);
-
---
--- Indeksy dla tabeli `reports`
---
-ALTER TABLE `reports`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `createdByUserId` (`createdByUserId`),
-  ADD KEY `projectId` (`projectId`);
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `TeamId` (`TeamId`),
+  ADD KEY `BudgetId` (`BudgetId`),
+  ADD KEY `ClientId` (`ClientId`);
 
 --
 -- Indeksy dla tabeli `taskassignments`
@@ -293,7 +250,7 @@ ALTER TABLE `teams`
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`Id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -303,49 +260,37 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `budgets`
 --
 ALTER TABLE `budgets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `equipment`
 --
 ALTER TABLE `equipment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `issues`
 --
 ALTER TABLE `issues`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `logs`
---
-ALTER TABLE `logs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `materials`
 --
 ALTER TABLE `materials`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `notifications`
+-- AUTO_INCREMENT for table `progressreports`
 --
-ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `progressreports`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `projects`
 --
 ALTER TABLE `projects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `reports`
---
-ALTER TABLE `reports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tasks`
@@ -363,7 +308,7 @@ ALTER TABLE `teams`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
@@ -373,61 +318,55 @@ ALTER TABLE `users`
 -- Constraints for table `equipment`
 --
 ALTER TABLE `equipment`
-  ADD CONSTRAINT `equipment_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `equipment_ibfk_1` FOREIGN KEY (`ProjectId`) REFERENCES `projects` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `issues`
 --
 ALTER TABLE `issues`
-  ADD CONSTRAINT `issues_ibfk_1` FOREIGN KEY (`reportedByUserId`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `issues_ibfk_2` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `issues_ibfk_1` FOREIGN KEY (`CreatedByUserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `issues_ibfk_2` FOREIGN KEY (`ProjectId`) REFERENCES `projects` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `materials`
 --
 ALTER TABLE `materials`
-  ADD CONSTRAINT `materials_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `materials_ibfk_1` FOREIGN KEY (`ProjectId`) REFERENCES `projects` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `notifications`
+-- Constraints for table `progressreports`
 --
-ALTER TABLE `notifications`
-  ADD CONSTRAINT `notifications_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+ALTER TABLE `progressreports`
+  ADD CONSTRAINT `progressreports_ibfk_1` FOREIGN KEY (`CreatedByUserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `progressreports_ibfk_2` FOREIGN KEY (`ProjectId`) REFERENCES `projects` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `projects`
 --
 ALTER TABLE `projects`
-  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`teamId`) REFERENCES `teams` (`Id`),
-  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`budgetId`) REFERENCES `budgets` (`id`),
-  ADD CONSTRAINT `projects_ibfk_3` FOREIGN KEY (`clientId`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `reports`
---
-ALTER TABLE `reports`
-  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`createdByUserId`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`projectId`) REFERENCES `projects` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`TeamId`) REFERENCES `teams` (`Id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`BudgetId`) REFERENCES `budgets` (`Id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `projects_ibfk_3` FOREIGN KEY (`ClientId`) REFERENCES `users` (`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `taskassignments`
 --
 ALTER TABLE `taskassignments`
-  ADD CONSTRAINT `taskassignments_ibfk_1` FOREIGN KEY (`TaskId`) REFERENCES `tasks` (`Id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `taskassignments_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `taskassignments_ibfk_1` FOREIGN KEY (`TaskId`) REFERENCES `tasks` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `taskassignments_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `teammembers`
 --
 ALTER TABLE `teammembers`
-  ADD CONSTRAINT `teammembers_ibfk_1` FOREIGN KEY (`TeamId`) REFERENCES `teams` (`Id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `teammembers_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `teammembers_ibfk_1` FOREIGN KEY (`TeamId`) REFERENCES `teams` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `teammembers_ibfk_2` FOREIGN KEY (`UserId`) REFERENCES `users` (`Id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `teams`
 --
 ALTER TABLE `teams`
-  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`ManagerId`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `teams_ibfk_1` FOREIGN KEY (`ManagerId`) REFERENCES `users` (`Id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
