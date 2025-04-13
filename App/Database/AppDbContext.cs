985eee73a82871705366ec25e4021c22c2a8f661
Patryk Stafecki <stafecki303@gmail.com>
2025-04-13 18:13:15 +0200

    Update db context

--- App/Database/AppDbContext.cs
+++ App/Database/AppDbContext.cs
@@ -1,21 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using ConstructionManagementApp.App.Models;
 using ConstructionManagementApp.App.Enums;
-using Task = ConstructionManagementApp.App.Models.Task;
-using System.Security.Cryptography.X509Certificates;
+using Task = ConstructionManagementApp.App.Models.Task; // Alias for Task model
 
 namespace ConstructionManagementApp.App.Database
 {
     internal class AppDbContext : DbContext
     {
-        public DbSet<User> Users { get; set; }      //odwołanie do tabeli Users w bazie danych
-        public DbSet<Task> Tasks { get; set; }     //odwołanie się do tabeli Tasks w bazie
-        public DbSet<TaskAssignment> TaskAssignments { get; set; }  //odwołanie się do tabeli TaskAssignments w bazie
+        public DbSet<User> Users { get; set; }
+        public DbSet<Task> Tasks { get; set; }
+        public DbSet<TaskAssignment> TaskAssignments { get; set; }
         public DbSet<Equipment> Equipment { get; set; }
         public DbSet<Budget> Budgets { get; set; }
         public DbSet<Team> Teams { get; set; }
@@ -26,202 +21,254 @@ namespace ConstructionManagementApp.App.Database
         public DbSet<ProgressReport> ProgressReports { get; set; }
         public DbSet<Log> Logs { get; set; }
         public DbSet<Message> Messages { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)   //elegnackie połączenie z baza danych
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            // Connection details (ensure these are correct for your environment)
             string server = "localhost";
             string database = "construction_management";
             string user = "root";
-            string password = "";
-            optionsBuilder.UseMySql($"server={server};database={database};user={user};password={password};", new MySqlServerVersion(new Version(10, 5, 9)));
+            string password = ""; // Consider using a configuration provider for sensitive data
+            optionsBuilder.UseMySql($"server={server};database={database};user={user};password={password};", new MySqlServerVersion(new Version(10, 4, 32))); // Match server version from SQL dump
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)  //konfiguracja modeli, żeby były poprawnie wpisywane
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
             modelBuilder.Entity<User>(entity =>
             {
+                entity.ToTable("users"); 
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Username).IsRequired();
-                entity.Property(e => e.Email).IsRequired();
-                entity.Property(e => e.PasswordHash).IsRequired();
+                entity.Property(e => e.Username).HasMaxLength(30).IsRequired();
+                entity.Property(e => e.Email).HasMaxLength(80).IsRequired();
+                entity.Property(e => e.PasswordHash).HasMaxLength(64).IsFixedLength().IsRequired(); 
                 entity.Property(e => e.Role)
-                      .HasConversion
-                      (
-                          v => v.ToString(),                                //konwersja enuma, bo EF nie obsługuje tego typu danych         
-                          v => (Role)Enum.Parse(typeof(Role), v)
-                      )
+                      .HasConversion(v => v.ToString(), v => (Role)Enum.Parse(typeof(Role), v))
                       .IsRequired();
-            });
-
-
-            modelBuilder.Entity<Task>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Title).IsRequired();
-                entity.Property(e => e.Description).IsRequired();
-                entity.Property(e => e.Priority)
-                      .HasConversion
-                      (
-                          v => v.ToString(),
-                          v => (TaskPriority)Enum.Parse(typeof(TaskPriority), v)
-                      )
-                      .IsRequired();
-                entity.Property(e => e.Progress)
-                      .HasConversion
-                      (
-                          v => v.ToString(),
-                          v => (TaskProgress)Enum.Parse(typeof(TaskProgress), v)
-                      )
-                      .IsRequired();
-                entity.HasOne<Project>()
-                        .WithMany()
-                        .HasForeignKey(e => e.ProjectId);                //klucz obcy reprezentujący id projektu
-            });
-
-            modelBuilder.Entity<TaskAssignment>(entity =>
-            {
-                entity.HasKey(e => new { e.UserId, e.TaskId });
-                entity.HasOne(e => e.User)
-                      .WithMany(e => e.TaskAssignments)
-                      .HasForeignKey(e => e.UserId);
-                entity.HasOne(e => e.Task)
-                        .WithMany(e => e.TaskAssignments)
-                        .HasForeignKey(e => e.TaskId);
-            }
-            );
-
-            modelBuilder.Entity<Equipment>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Name).IsRequired();
-                entity.Property(e => e.Status)
-                      .HasConversion
-                      (
-                          v => v.ToString(),
-                          v => (EquipmentStatus)Enum.Parse(typeof(EquipmentStatus), v)
-                      )
-                      .IsRequired();
-                entity.HasOne<Project>()                //kluczobcy ktory jest id tabeli project
-                        .WithMany()
-                        .HasForeignKey(e => e.ProjectId);
             });
 
             modelBuilder.Entity<Budget>(entity =>
             {
+                entity.ToTable("budgets");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.TotalAmount).IsRequired();
-                entity.Property(e => e.SpentAmount).IsRequired();
-            });
-            modelBuilder.Entity<Team>(entity =>
-            {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.Name).IsRequired();
-                entity.HasOne<User>()                //klucz obcy ktory jest id managera z tabeli user
-                      .WithMany()
-                      .HasForeignKey(e => e.ManagerId);
+                entity.Property(e => e.TotalAmount).HasColumnType("decimal(10,2)").IsRequired();
+                entity.Property(e => e.SpentAmount).HasColumnType("decimal(10,2)").IsRequired();
             });
 
-            modelBuilder.Entity<TeamMembers>(entity =>
+            modelBuilder.Entity<Team>(entity =>
             {
-                entity.HasKey(e => new { e.UserId, e.TeamId });
-                entity.HasOne(e => e.User)
-                      .WithMany(e => e.TeamMembers)
-                      .HasForeignKey(e => e.UserId);                //tabela lacznikowa miedzy user a team
-                entity.HasOne(e => e.Team)
-                        .WithMany(e => e.TeamMembers)
-                        .HasForeignKey(e => e.TeamId);
-            });                                    
+                entity.ToTable("teams");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Name).HasMaxLength(255).IsRequired();
+
+                entity.HasOne<User>()
+                      .WithMany()
+                      .HasForeignKey(e => e.ManagerId)
+                      .IsRequired(false)
+                      .OnDelete(DeleteBehavior.ClientSetNull);
+            });
 
             modelBuilder.Entity<Project>(entity =>
             {
+                entity.ToTable("projects");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Name).IsRequired();
-                entity.Property(e => e.Description).IsRequired();
+                entity.Property(e => e.Name).HasMaxLength(255).IsRequired();
+                entity.Property(e => e.Description).HasColumnType("text").IsRequired();
+
                 entity.HasOne<Team>()
                       .WithMany()
-                      .HasForeignKey(e => e.TeamId);                //klucze obce reprezentujące id z tabeli team, budget oraz user
+                      .HasForeignKey(e => e.TeamId)
+                      .IsRequired(false) 
+                      .OnDelete(DeleteBehavior.ClientSetNull);
+
                 entity.HasOne<Budget>()
-                        .WithMany()
-                        .HasForeignKey(e => e.BudgetId);
+                      .WithMany()
+                      .HasForeignKey(e => e.BudgetId)
+                      .IsRequired(false) 
+                      .OnDelete(DeleteBehavior.ClientSetNull);
+
                 entity.HasOne<User>()
-                        .WithMany()
-                        .HasForeignKey(e => e.ClientId);
+                      .WithMany()
+                      .HasForeignKey(e => e.ClientId)
+                      .IsRequired(false) 
+                      .OnDelete(DeleteBehavior.ClientSetNull);
             });
 
-            modelBuilder.Entity<Issue>(entity =>
+            modelBuilder.Entity<Equipment>(entity =>
             {
+                entity.ToTable("equipment");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Title).IsRequired();
-                entity.Property(e => e.Content).IsRequired();
-                entity.Property(e => e.CreatedAt).IsRequired();
-                entity.Property(e => e.Priority)
-                    .HasConversion(
-                        v => v.ToString(),
-                        v => (TaskPriority)Enum.Parse(typeof(TaskPriority), v)
-                    )
-                    .IsRequired();
+                entity.Property(e => e.Name).HasMaxLength(255).IsRequired();
                 entity.Property(e => e.Status)
-                    .HasConversion(
-                        v => v.ToString(),
-                        v => (IssueStatus)Enum.Parse(typeof(IssueStatus), v)
-                    )
-                    .IsRequired();
-                entity.HasOne<User>()
-                    .WithMany()
-                    .HasForeignKey(e => e.CreatedByUserId)
-                    .OnDelete(DeleteBehavior.Cascade);
+                      .HasMaxLength(50) 
+                      .HasConversion(v => v.ToString(), v => (EquipmentStatus)Enum.Parse(typeof(EquipmentStatus), v))
+                      .IsRequired();
+
+
                 entity.HasOne<Project>()
-                    .WithMany()
-                    .HasForeignKey(e => e.ProjectId)
-                    .OnDelete(DeleteBehavior.Cascade);
+                      .WithMany() 
+                      .HasForeignKey(e => e.ProjectId)
+                      .IsRequired(false) 
+                      .OnDelete(DeleteBehavior.Cascade); 
             });
 
             modelBuilder.Entity<Material>(entity =>
             {
+                entity.ToTable("materials");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Name).IsRequired();
+                entity.Property(e => e.Name).HasMaxLength(255).IsRequired();
                 entity.Property(e => e.Quantity).IsRequired();
-                entity.Property(e => e.Unit).IsRequired();
+                entity.Property(e => e.Unit).HasMaxLength(50).IsRequired();
+
                 entity.HasOne<Project>()
-                      .WithMany()
-                      .HasForeignKey(e => e.ProjectId);
+                      .WithMany() 
+                      .HasForeignKey(e => e.ProjectId)
+                      .IsRequired(false) 
+                      .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.ToTable("tasks");
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.Title).HasMaxLength(255).IsRequired();
+                entity.Property(e => e.Description).HasColumnType("text").IsRequired(); 
+                entity.Property(e => e.Priority)
+                      .HasConversion(v => v.ToString(), v => (TaskPriority)Enum.Parse(typeof(TaskPriority), v))
+                      .IsRequired();
+                entity.Property(e => e.Progress)
+                      .HasConversion(v => v.ToString(), v => (TaskProgress)Enum.Parse(typeof(TaskProgress), v))
+                      .IsRequired();
+
+                
+                entity.HasOne<Project>()
+                      .WithMany() 
+                      .HasForeignKey(e => e.ProjectId)
+                      .IsRequired() 
+                      .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            
+            modelBuilder.Entity<TaskAssignment>(entity =>
+            {
+                entity.ToTable("taskassignments");
+                entity.HasKey(e => new { e.TaskId, e.UserId }); 
+
+                entity.HasOne(e => e.Task)
+                      .WithMany(t => t.TaskAssignments) 
+                      .HasForeignKey(e => e.TaskId)
+                      .OnDelete(DeleteBehavior.Cascade); 
+
+
+                entity.HasOne(e => e.User)
+                      .WithMany(u => u.TaskAssignments) 
+                      .HasForeignKey(e => e.UserId)
+                      .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+            
+            modelBuilder.Entity<TeamMembers>(entity =>
+            {
+                entity.ToTable("teammembers");
+                entity.HasKey(e => new { e.TeamId, e.UserId }); 
+
+                
+                entity.HasOne(e => e.Team)
+                      .WithMany(t => t.TeamMembers) 
+                      .HasForeignKey(e => e.TeamId)
+                      .OnDelete(DeleteBehavior.Cascade); 
+
+                
+                entity.HasOne(e => e.User)
+                      .WithMany(u => u.TeamMembers) 
+                      .HasForeignKey(e => e.UserId)
+                      .OnDelete(DeleteBehavior.Cascade); 
+            });
+
+           
+            modelBuilder.Entity<Issue>(entity =>
+            {
+                entity.ToTable("issues");
+                entity.HasKey(e => e.Id); 
+                entity.Property(e => e.Title).HasMaxLength(255).IsRequired(); 
+                entity.Property(e => e.Content).HasColumnType("text").IsRequired();
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime").IsRequired();
+                entity.Property(e => e.ResolvedAt).HasColumnType("datetime").IsRequired(false); 
+                entity.Property(e => e.Priority)
+                    .HasConversion(v => v.ToString(), v => (TaskPriority)Enum.Parse(typeof(TaskPriority), v))
+                    .IsRequired();
+                entity.Property(e => e.Status)
+                    .HasConversion(v => v.ToString(), v => (IssueStatus)Enum.Parse(typeof(IssueStatus), v))
+                    .IsRequired();
+
+                
+                entity.HasOne<User>()
+                    .WithMany() 
+                    .HasForeignKey(e => e.CreatedByUserId)
+                    .IsRequired() 
+                    .OnDelete(DeleteBehavior.ClientSetNull); 
+
+               
+                entity.HasOne<Project>()
+                    .WithMany() 
+                    .HasForeignKey(e => e.ProjectId)
+                    .IsRequired() 
+                    .OnDelete(DeleteBehavior.Cascade); 
             });
 
             modelBuilder.Entity<ProgressReport>(entity =>
             {
+                entity.ToTable("progressreports");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Title).IsRequired();
-                entity.Property(e => e.Content).IsRequired();
-                entity.Property(e => e.CreatedAt).IsRequired();
-                entity.Property(e => e.CompletionPercentage)
-                    .IsRequired(); // Ensure CompletionPercentage is required
+
+                entity.Property(e => e.Title).HasMaxLength(255).IsRequired();
+                entity.Property(e => e.Content).HasColumnType("text").IsRequired();
+                entity.Property(e => e.CreatedAt).HasColumnType("datetime").IsRequired();
+                entity.Property(e => e.CompletionPercentage).IsRequired();
+
                 entity.HasOne<User>()
-                    .WithMany()
+                    .WithMany() 
                     .HasForeignKey(e => e.CreatedByUserId)
-                    .OnDelete(DeleteBehavior.Cascade);
+                    .IsRequired() 
+                    .OnDelete(DeleteBehavior.Cascade); 
+
                 entity.HasOne<Project>()
-                    .WithMany()
+                    .WithMany() 
                     .HasForeignKey(e => e.ProjectId)
-                    .OnDelete(DeleteBehavior.Cascade);
+                    .IsRequired() 
+                    .OnDelete(DeleteBehavior.Cascade); 
             });
 
+            // Log Configuration
             modelBuilder.Entity<Log>(entity =>
             {
+                entity.ToTable("logs");
                 entity.HasKey(e => e.Id);
-                entity.Property(e => e.Message).IsRequired();
-                entity.Property(e => e.Timestamp).IsRequired();
+                entity.Property(e => e.Message).HasColumnType("text").IsRequired();
+                entity.Property(e => e.Timestamp).HasColumnType("datetime").IsRequired();
             });
 
+            // Message Configuration
             modelBuilder.Entity<Message>(entity =>
             {
-                entity.HasKey(e => e.Id);
-                entity.Property(e => e.SenderId).IsRequired();
-                entity.Property(e => e.ReceiverId).IsRequired();
-                entity.Property(e => e.Content).IsRequired();
-                entity.Property(e => e.SentAt).IsRequired();
+                entity.ToTable("messages");
+                entity.HasKey(e => e.Id); 
+                entity.Property(e => e.Content).HasColumnType("text").IsRequired();
+                entity.Property(e => e.SentAt).HasColumnType("datetime").IsRequired();
+
+                entity.HasOne<User>()
+                      .WithMany() 
+                      .HasForeignKey(e => e.SenderId)
+                      .IsRequired() 
+                      .OnDelete(DeleteBehavior.Restrict); 
+
+                entity.HasOne<User>()
+                     .WithMany() 
+                     .HasForeignKey(e => e.ReceiverId)
+                     .IsRequired() 
+                     .OnDelete(DeleteBehavior.Restrict); 
             });
-
-
         }
     }
 }
