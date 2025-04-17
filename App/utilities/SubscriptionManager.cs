c570cbe5ed41334d88d89a72c8b4b338a0f2e265
Filip Lubka <filip.lubka2007@gmail.com>
2025-04-17 13:37:09 +0200

    Created subscription manager to get program.cs clean

---
+++ App/utilities/SubscriptionManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConstructionManagementApp.App.Controllers;
+using ConstructionManagementApp.App.Services;
+using ConstructionManagementApp.Services;
+
+namespace ConstructionManagementApp.App.Utilities
+{
+    internal static class EventSubscriptionManager
+    {
+        public static void SubscribeEvents(
+            AuthenticationService authenticationService,
+            UserController userController,
+            BudgetController budgetController,
+            EquipmentController equipmentController,
+            IssueController issueController,
+            MaterialController materialController,
+            MessageController messageController,
+            ProgressReportController progressReportController,
+            LogService logService)
+        {
+            // Subscribe to events
+            authenticationService.UserLoggedIn += logService.OnActionOccurred;
+            userController.UserAdded += logService.OnActionOccurred;
+            userController.UserUpdated += logService.OnActionOccurred;
+            userController.UserDeleted += logService.OnActionOccurred;
+            budgetController.BudgetAdded += logService.OnActionOccurred;
+            budgetController.BudgetUpdated += logService.OnActionOccurred;
+            budgetController.BudgetDeleted += logService.OnActionOccurred;
+            equipmentController.EquipmentAdded += logService.OnActionOccurred;
+            equipmentController.EquipmentUpdated += logService.OnActionOccurred;
+            equipmentController.EquipmentDeleted += logService.OnActionOccurred;
+            issueController.IssueAdded += logService.OnActionOccurred;
+            issueController.IssueUpdated += logService.OnActionOccurred;
+            issueController.IssueDeleted += logService.OnActionOccurred;
+            materialController.MaterialAdded += logService.OnActionOccurred;
+            materialController.MaterialUpdated += logService.OnActionOccurred;
+            materialController.MaterialDeleted += logService.OnActionOccurred;
+            messageController.MessageSent += logService.OnActionOccurred;
+            progressReportController.ProgressReportAdded += logService.OnActionOccurred;
+            progressReportController.ProgressReportUpdated += logService.OnActionOccurred;
+            progressReportController.ProgressReportDeleted += logService.OnActionOccurred;
+        }
+    }
+}
